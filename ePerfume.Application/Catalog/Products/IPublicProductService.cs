781d73b986e07ce567398fe58db34befc88c4fe6
Ngô Đại Dương <ngodaiduong116@gmail.com>
2022-03-06 16:48:24 +0700

    Done Struct

---
+++ ePerfume.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,16 @@
+﻿using ePerfume.ViewModels.Catalog.Products;
+using ePerfume.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePerfume.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        public Task<PageResult<ProductViewModel>> GetAllByCategoryId(GetPublicProductPagingRequest request);
+        public Task<List<ProductViewModel>> GetAll();
+    }
+}
