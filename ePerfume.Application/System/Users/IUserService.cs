107ae67ecd954af59ce4da48eb52718e07d9ccb0
Ngô Đại Dương <ngodaiduong116@gmail.com>
2022-03-15 00:00:08 +0700

    l1

---
+++ ePerfume.Application/System/Users/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using ePerfume.ViewModels.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePerfume.Application.System.Users
+{
+    public interface IUserService
+    {
+        Task<string> Authenticate(LoginRequest request);
+
+        Task<bool> Register(RegisterRequest request);
+    }
+}
---
+++ ePerfume.Application/System/Users/UserService.cs
@@ -0,0 +1,82 @@
+﻿using ePerfume.Data.Entities;
+using ePerfume.Utilities.Exceptions;
+using ePerfume.ViewModels.System.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePerfume.Application.System.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly RoleManager<Role> _roleManager;
+        private readonly IConfiguration _config;
+
+        public UserService(UserManager<User> userManager, SignInManager<User> signInmanager, RoleManager<Role> roleManager, IConfiguration config)
+        {
+            _userManager = userManager;
+            _signInManager = signInmanager;
+            _roleManager = roleManager;
+            _config = config;
+        }
+
+        public async Task<string> Authenticate(LoginRequest request)
+        {
+            var user = await _userManager.FindByNameAsync(request.UserName);
+            if (user == null)
+            {
+                return null;
+            }
+            var result = await _signInManager.PasswordSignInAsync(user, request.Password, request.RememberMe, true);
+            if (!result.Succeeded)
+            {
+                return null;
+            }
+            var roles = _userManager.GetRolesAsync(user);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Role, string.Join(";", roles))
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Tokens:Issuer"],
+                _config["Tokens:Issuer"],
+                claims,
+                expires: DateTime.Now.AddHours(3),
+                signingCredentials: creds
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public async Task<bool> Register(RegisterRequest request)
+        {
+            var user = new User()
+            {
+                Dob = request.D0b,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                UserName = request.UserName,
+                PhoneNumber = request.PhoneNumber,
+            };
+            var result = await _userManager.CreateAsync(user);
+            if (result.Succeeded)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
