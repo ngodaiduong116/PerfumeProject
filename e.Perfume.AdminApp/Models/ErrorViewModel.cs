9df90109c02d3668ff40bf48e8a9ddfb99847358
Ngô Đại Dương <ngodaiduong116@gmail.com>
2022-04-08 09:01:30 +0700

    ok

---
+++ e.Perfume.AdminApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace e.Perfume.AdminApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
